d1fa700ad52e01809560cf9c4012c07eeacff54c
Swapnil Ghosh <swapnil.ghosh@eduonix.com>
2019-07-29 18:37:04 +0530

    electron js implementation

---
+++ electron_js/main.js
@@ -0,0 +1,18 @@
+	
+$('.frontSubmitBtn').on('click',()=>{
+	$('.bannerUrl').each(function(){
+		$(".result").append($(this).val()+"<br/>")
+	})
+	crateHtmlFile()
+})
+
+// Creating HTML
+function crateHtmlFile(){
+	fs.writeFile('xyz.html', "Hey there!", function(err) {
+    if(err) {
+        return alert(err)
+    }
+    console.log("The file was saved!")
+	}) 
+}
+
--- project.py
+++ project.py
@@ -12,6 +12,32 @@ m=tkinter.Tk()
 m.title('zip creator Extractor')
 m.geometry("600x300")
 
+# --- create canvas with scrollbar ---
+
+def on_configure(event):
+    # update scrollregion after starting 'mainloop'
+    # when all widgets are in canvas
+    canvas.configure(scrollregion=canvas.bbox('all'))
+
+canvas = tkinter.Canvas(m)
+canvas.pack(side=tkinter.LEFT)
+# --- put frame in canvas ---
+
+frame = tkinter.Frame(canvas)
+canvas.create_window((0,0), window=frame, anchor='nw')
+
+# --- add widgets in frame ---
+
+scrollbar = tkinter.Scrollbar(frame, command=canvas.yview)
+scrollbar.pack(side=tkinter.LEFT, fill='y')
+
+canvas.configure(width=200, height=200, yscrollcommand = scrollbar.set)
+
+# update scrollregion after starting 'mainloop'
+# when all widgets are in canvas
+canvas.bind('<Configure>', on_configure)
+
+
 variable = IntVar()
 startTxt = """<head>
     <title>Eduonix</title>
@@ -27,18 +53,27 @@ startTxt = """<head>
 option = StringVar()
 my_list = []
 
-E = Entry(m, bd=5, width=50)
+E = Entry(frame, bd=5, width=50)
 
 path ='June19-Mailing.xlsx'
 
 
-l1 = Label(m, text = "Please Enter Number of Section")
+l1 = Label(frame, text = "Please Enter Number of Section")
 l1.pack(side= LEFT)
 l1.place(x=10,y=10)
 # button
-E1= Entry(m, bd=5, width=5)
+E1= Entry(frame, bd=5, width=5)
 E1.pack(side= LEFT)
 E1.place(x=80,y=10)
+
+
+def getEntryField():
+	entryStrip = []
+	utmLinkLabel = Label(frame, text = "Enter Utm Link",wraplength=100).pack(side=LEFT)
+	EntryStripUtm = Entry(frame, bd=2, width=100).pack(side=tkinter.LEFT)
+
+	utmLinkLabel = Label(frame, text = "Enter Img Link",wraplength=100).pack(side=tkinter.LEFT)
+	EntryStripImg = Entry(frame, bd=2, width=100).pack(side=tkinter.LEFT)
 
 
 def getNumber():
@@ -50,27 +85,29 @@ def getNumber():
 		number = int(number,10)
 		entries = []
 		for i in range(0,number):
+				buttonStrip = Button(frame, text='Add Strip', command = lambda : getEntryField()).pack(side = TOP)
+
 				newrow = []
 				# textCol = str(j+1) + "1 Coloumn"
 				
-				l2 = Label(m, text = "Enter number of coloumn",wraplength=100).grid(row=i, column=1)
+				l2 = Label(frame, text = "Enter number of coloumn",wraplength=100).grid(row=i, column=1)
 				# button
-				E2 = Entry(m, bd=5, width=5)
+				E2 = Entry(frame, bd=5, width=5)
 				E2.grid(row=i, column=2)
 
 
-				l3 = Label(m, text = "Start and End Row Number",wraplength=100).grid(row=i, column=3)
+				l3 = Label(frame, text = "Start and End Row Number",wraplength=100).grid(row=i, column=3)
 				# button
-				E3 = Entry(m, bd=5, width=5)
+				E3 = Entry(frame, bd=5, width=5)
 				E3.grid(row=i, column=4) 
-				E4 = Entry(m, bd=5, width=5)
+				E4 = Entry(frame, bd=5, width=5)
 				E4.grid(row=i, column=5)
 
-				l4 = Label(m, text = "Start and End Coloumn Number",wraplength=100).grid(row=i, column=6)
+				l4 = Label(frame, text = "Start and End Coloumn Number",wraplength=100).grid(row=i, column=6)
 				# button
-				E5 = Entry(m, bd=5, width=5)
+				E5 = Entry(frame, bd=5, width=5)
 				E5.grid(row=i, column=7) 
-				E6 = Entry(m, bd=5, width=5)
+				E6 = Entry(frame, bd=5, width=5)
 				E6.grid(row=i, column=8)
 
 
@@ -83,7 +120,7 @@ def getNumber():
 
 
 
-		button1 = Button(m,text='submit', command = lambda : getValues()).grid(row=i+1, column=3)
+		button1 = Button(frame, text='submit', command = lambda : getValues()).grid(row=i+1, column=3)
 
 	def getValues():
 		for entry in entries:
@@ -102,19 +139,8 @@ def getNumber():
 					C2 = obj.get()
 			tg.genertateTemplate(path,selection,R1,R2,C1,C2)
 
-def moreStrip():
-	gridCol = gridCol + 1
-	S = Entry(m, bd=5, width=5)
-	S.grid(row=gridCol, column=2)
-
-gridCol = 1
-Sl1 = Label(m, text = "Strip 1",wraplength=100).grid(row=1, column=1)
-S = Entry(m, bd=5, width=5)
-S.grid(row=gridCol, column=2)
-
-button = Button(m,text='submit',command = moreStrip)
 		
-button = Button(m,text='submit',command = getNumber)
+button = Button(frame, text='submit',command = getNumber)
 button.pack(side = BOTTOM)
 button.place(x=200,y=120)
 
