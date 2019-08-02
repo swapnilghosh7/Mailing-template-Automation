e0a058881d3c62ec9235e8f81c7e4b3a464a8ec3
Swapnil Ghosh <swapnil.ghosh@eduonix.com>
2019-08-02 18:33:45 +0530

    sync issue

--- electron_js/templateGenerator.py
+++ electron_js/templateGenerator.py
@@ -1,11 +1,10 @@
-def genertateTemplate(path,selection,R1,R2,C1=1,C2=2,templateName):
+def genertateTemplate(path,selection,R1,R2,C1,C2,templateName):
 
 	selection = int(selection,10)
 	R1 = int(R1,10)
 	R2 = int(R2,10)
 	C1 = int(C1,10)
 	C2 = int(C2,10)
-	print('insideGT')
 	import xlrd
 	import datetime
 
@@ -16,8 +15,8 @@ def genertateTemplate(path,selection,R1,R2,C1=1,C2=2,templateName):
 	# sheet = wb.sheet_by_index(1)
 	x = datetime.datetime.now()
 	dateToday = x.strftime("%d")+x.strftime("%b")+x.strftime("%y")
-	templateName = "template"+dateToday
-
+	# templateName = "template"+dateToday
+	templateName = "temp"
 
 	doubleColHtml = """<td align='center'><table>\n"""
 	if(selection > 1):
@@ -25,20 +24,20 @@ def genertateTemplate(path,selection,R1,R2,C1=1,C2=2,templateName):
 	else:
 		data = """<tr><td colspan="3"> &nbsp;</td></tr>\n"""
 
-	a =  open(templateName, "a")
+	a =  open(templateName+".txt", "w+")
 	a.write(data)
 	for i in range(R1-1,R2-1,selection):
 		a.write("<tr>")
 		for j in range(0,selection):
 			col = i + j
-			data = """\n<td><a href='"""+sh.cell_value(col,C1)+"""'><img src='"""+sh.cell_value(col,C2)+"""'></a></td>"""
+			data = """\n<td><a href='"""+sh.cell_value(col,C1-1)+"""'><img src='"""+sh.cell_value(col,C2-1)+"""'></a></td>"""
 			if(selection>1):
 				data = data + """ <td> &nbsp;</td> """
 
-			a =  open(templateName+".html", "a")
+			a =  open(templateName+".txt", "a")
 			a.write(data)
 		a.write("""</tr>\n""")
 
 	if(selection > 1):
-		a =  open(templateName+".html", "a")
+		a =  open(templateName+".txt", "a")
 		a.write("""</table></td>""")
